2e41d42f61ff89de91b28cb40ec1add7cfb6de17
Sebastian Peralta <peraltas@seas.upenn.edu>
2024-01-13 01:18:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(name="octo", packages=["octo"])
+setup(
+    name="octo",
+    version="0.0.1",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+      "jax==0.4.23",
+      "jaxlib==0.4.23",
+      "opencv-python==4.9.0.80",
+      "flax==0.7.5",
+      "distrax==0.1.5",
+      "einops==0.7.0",
+      "huggingface-hub==0.20.2",
+      "transformers==4.36.2",
+      "git+https://github.com/google/array_record.git@2ac1d904f6be31e5aa2f09549774af65d84bff5a",
+      "git+https://github.com/kvablack/dlimp@d08da3852c149548aaa8551186d619d87375df08",
+    ],
+)
